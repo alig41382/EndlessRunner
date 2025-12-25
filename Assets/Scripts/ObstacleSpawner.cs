eac4f5f4d9d017fdda3ba641bc77ac2c9ee41c2d
alig41382 <gholamiali41380@gmail.com>
2025-12-25 20:11:19 +0330

    Added the Obstacle And the Spawner

---
+++ Assets/Scripts/ObstacleSpawner.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+
+public class ObstacleSpawner : MonoBehaviour
+{
+    [SerializeField]
+    GameObject obstaclePrefab;
+
+    [SerializeField]
+    private Transform player;
+
+    [SerializeField]
+    private float minDistance = 4f;
+
+    [SerializeField]
+    private float maxDistance = 7f;
+
+    private float laneOffset = 2f;
+
+    private float nextSpawnZ;
+
+    void Start()
+    {
+        nextSpawnZ = transform.position.z;
+    }
+
+    void Update()
+    {
+        if (player.position.z + 20f > nextSpawnZ)
+        {
+            SpawnObstacle();
+        }
+    }
+
+    void SpawnObstacle()
+    {
+        int laneIndex = Random.Range(0, 3);
+        float xPos = (laneIndex - 1) * laneOffset;
+
+        Vector3 spawnPos = new Vector3(xPos, 0.5f, nextSpawnZ);
+
+        Instantiate(obstaclePrefab, spawnPos, Quaternion.identity);
+
+        // Increase Z for next spawn
+        nextSpawnZ += Random.Range(minDistance, maxDistance);
+    }
+}
