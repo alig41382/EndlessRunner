793b57dd1427923841f2f18ddb47b96fccc60f09
alig41382 <gholamiali41380@gmail.com>
2025-12-25 21:18:33 +0330

    Added the GameManager(Have To Add Panels)

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance { get; private set; }
+
+    public bool isGameOver = false;
+
+    [Header("References")]
+    [SerializeField]
+    PlayerController playerController;
+
+    [SerializeField]
+    ObstacleSpawner spawner;
+
+    [SerializeField]
+    GameObject gameOverPanel;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Awake()
+    {
+        Instance = this;
+    }
+
+    void Start()
+    {
+        gameOverPanel.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update() { }
+
+    public void GameOver()
+    {
+        if (isGameOver)
+            return;
+        isGameOver = true;
+        playerController.enabled = false;
+        spawner.enabled = false;
+        gameOverPanel.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+    public void Restart()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
